ea1f374a6d1699694779b15b93a919e8a29e22da
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-31 06:49:55 +0300

    Add project files.

---
+++ AbstractFactoryRealExample/Movements/IMovement.cs
@@ -0,0 +1,10 @@
+﻿namespace AbstractFactoryRealExample.Movements
+{
+    /// <summary>
+    ///  Базовый интерфейс для движения
+    /// </summary>
+    interface IMovement
+    {
+        void Start();
+    }
+}
---
+++ AbstractFactoryRealExample/Weapons/IWeapon.cs
@@ -0,0 +1,10 @@
+﻿namespace AbstractFactoryRealExample.Weapons
+{
+    /// <summary>
+    ///  Базовый интерфейс для оружия
+    /// </summary>
+    interface IWeapon
+    {
+        void Attack();
+    }
+}
